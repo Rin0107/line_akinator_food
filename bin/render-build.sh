9e0acb43522a20dc6e0e051a332eedf92d490e81
Rin0107 <rinpei09107@gmail.com>
2022-11-28 14:49:19 +0900

    render-build.shでdb:migrate:resetとdb:seed追加してみた

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,6 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rake db:migrate:reset
+bundle exec rake db:seed SECTION=f_1
+bundle exec rake db:seed SECTION=q_1
