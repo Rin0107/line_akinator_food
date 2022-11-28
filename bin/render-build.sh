53634eec53f78f48d9c34b1d9b3a7aa2eb810586
Rin0107 <rinpei09107@gmail.com>
2022-11-28 15:14:04 +0900

    render-build.shでseedの書き方訂正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,4 +7,7 @@ bundle exec rake assets:precompile
 bundle exec rake assets:clean
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
 bundle exec rake db:seed SECTION=f_2
-bundle exec rake db:seed SECTION=q_2, q_3, q_4, q_5
+bundle exec rake db:seed SECTION=q_2
+bundle exec rake db:seed SECTION=q_3
+bundle exec rake db:seed SECTION=q_4
+bundle exec rake db:seed SECTION=q_5
