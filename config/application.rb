aaaffb7b4fced96da23c0034f7e25a526a541467
Rin0107 <rinpei09107@gmail.com>
2022-11-28 14:04:24 +0900

    config.host << ホスト名訂正

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,6 @@ module LineAkinatorFood
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.hosts << 'line_akinator_food_2.onrender.com'
+    config.hosts << 'line-akinator-food-2.onrender.com'
   end
 end
