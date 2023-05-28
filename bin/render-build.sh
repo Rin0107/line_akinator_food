b3375ad57d122857e0646127a2ae5012d5c5e437
Rin0107 <rinpei09107@gmail.com>
2023-05-28 20:49:38 +0900

    solutionを減らしてquestion追加

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,9 +7,11 @@ bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
 # 以下seed挿入完了済み。次回以降の挿入はbundle exec rake db:seed SECTION=*で挿入する。
-# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
 # bundle exec rake db:seed SECTION=f_2
 # bundle exec rake db:seed SECTION=q_2
 # bundle exec rake db:seed SECTION=q_3
 # bundle exec rake db:seed SECTION=q_4
 # bundle exec rake db:seed SECTION=q_5
+bundle exec rake db:seed SECTION=f_4
+bundle exec rake db:seed SECTION=q_6
